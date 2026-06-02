bf06293e84af188b2a3914b3a546b13286bbf71b
xsh10256 <26409670200@qq.com>
2026-06-02 18:29:40 +0800

    modified :  src/GPT/gpt_fan.c

--- src/GPT/gpt_fan.c
+++ src/GPT/gpt_fan.c
@@ -1,2 +1,8 @@
 #include "gpt_fan.h"
-//imhwj
+
+void gpt_fan_init(void)
+{
+    R_open(&g_timer_fan_ctrl, &g_timer_fan_cfg);
+    R_timer_fan_ctrl_callback_set(&g_timer_fan_ctrl, gpt_fan_callback, NULL);
+    R_timer_fan_ctrl_start(&g_timer_fan_ctrl);
+}
